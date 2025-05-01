77f982114fae7940d4d25b6770c4ee3da4a7ae60
Stephen Lewis <st.rayis1085@gmail.com>
2025-05-01 10:52:20 -0400

    fresh start

---
+++ public/js/config.js
@@ -0,0 +1,4 @@
+const config = {
+    port: process.env.port || 3000
+};
+export default config;
---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import morgan from "morgan";
+import config from "#config";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(express.static("./public"));
+
+app.listen(config.port, () => {
+    console.log(`Listenin @ ${config.port}`)
+})
