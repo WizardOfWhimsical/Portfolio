4a8b072335bcd939c8c8a5be1401d8a01d7d4568
Stephen Lewis <st.rayis1085@gmail.com>
2025-05-02 12:46:55 -0400

    fixed gradient and links

--- server.js
+++ server.js
@@ -12,3 +12,8 @@ app.use(express.static("./public"));
 app.listen(config.port, () => {
     console.log(`Listenin @ ${config.port}`)
 })
+
+
+app.use((req, res, next) => {
+    res.status(404).send("404: Page Not Found, Sorry")
+})
