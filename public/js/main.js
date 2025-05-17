298d5d1a75ca13401fad15970e2c053e6914e3b8
Stephen Lewis <st.rayis1085@gmail.com>
2025-05-17 13:26:13 -0400

    did way to much with out saving this thing

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+/*
+[]so i want to make a click event that allows me to show images from my index to expand them for better viewing.
+[]also i want to maybe round the edges of the images and put a border around them that will allow me to fade the edges into the background.
+*/
